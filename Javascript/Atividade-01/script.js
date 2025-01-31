d7b742c54c5b60597bd418c70847f81f73e2710a
giihsousaa <giovannasousa197@gmail.com>
2025-01-31 15:55:54 -0300

    Atividade-01

---
+++ Javascript/Atividade-01/script.js
@@ -0,0 +1,55 @@
+console.log("Bom Dia Mundo!!!");
+    alert("Bom Dia Mundo!! ")
+  
+
+    var numero1 = (prompt("Digite seu nome"))
+   var numero2 = (prompt("Digite seu sobrenome "))
+   
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
+    
+    var numero1 = Number.parseFloat(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseFloat(prompt("Digite o segundo numero"))
+    //Verificando o tipo de variavel
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
+
+    var numero1 = Number.parseFloat(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseFloat(prompt("Digite o segundo numero"))
+    //Verificando o tipo de variavel
+    console.log(typeof(numero1))
+    alert(numero1 - numero2)
+
+    var numero1 = Number.parseFloat(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseFloat(prompt("Digite o segundo numero"))
+    //Verificando o tipo de variavel
+    console.log(typeof(numero1))
+    alert(numero1 * numero2)
+
+    var numero1 = Number.parseFloat(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseFloat(prompt("Digite o segundo numero"))
+    //Verificando o tipo de variavel
+    console.log(typeof(numero1))
+    alert(numero1 / numero2)
+
+    var numero1 = Number.parseFloat(prompt("Digite um numero"))
+    //Verificando o tipo de variavel
+    console.log(typeof(numero1))
+    alert(numero1 **3)
+
+    var numero = Number.parseInt(prompt("Digite a temperatura em F"));
+alert((numero - 32) * (5/9));
+
+    var C = parseFloat(prompt("Capital inicial (R$):"));
+    var i = parseFloat(prompt("Taxa de juros mensal (%):")) / 100;
+    var t = parseInt(prompt("Tempo do investimento (meses):"), 10);
+
+    var M = C * (1 + i) ** t;
+
+    alert("Montante acumulado: R$ " + M.toFixed(2));
+
+
+
+
+
+  
