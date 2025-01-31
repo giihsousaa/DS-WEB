8eae1817257273a017abab6859f16c2ee2c57702
giihsousaa <giovannasousa197@gmail.com>
2025-01-31 12:17:13 -0300

    Aula-01

---
+++ Javascript/Aula-01/script.js
@@ -0,0 +1,24 @@
+console.log("Olá Mundo");
+
+//exemplo de alert()
+    //alert("Olá Mundo 1")
+
+//exemplo de confirm()
+    //var teste = confirm("Olá Mundo")
+    //exibindo a opcao escolhida pelo usuário
+   // console.log(teste)
+
+//exemplo de Prompt()
+    //var testePrompt = prompt("Digite seu nome")
+    //exibindo o nome do usuário
+    //console.log(testePrompt)
+    //alert (testePrompt)
+
+    //teste de adicao
+    var numero1 = Number.parsenInt(prompt("Digite seu primeiro numero"))
+    var numero2 = Number.parsenInt(prompt("Digite seu segundo numero"))
+    alert(result)
+    console.log(result);
+    //verificanto o tipo de variavel
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
